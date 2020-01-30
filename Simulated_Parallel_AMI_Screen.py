4fe20055cc26c1e271994004231ea14dbaaff2b3
Calum Hand <crh53@bath.ac.uk>
2020-01-30 12:45:31 +0000

    updating main file to run with the code changes...

--- Simulated_Parallel_AMI_Screen.py
+++ Simulated_Parallel_AMI_Screen.py
@@ -5,10 +5,17 @@ from uuid import uuid4
 import json
 
 import pandas as pd
-import BOGP
+from Prospector_2 import prospector2
 
 from AmiSimTools.DataTriage import DataTriageCSV
 from AmiSimTools.SimScreen import SimulatedScreenerParallel
+
+
+def load_costs(json_path):
+    with open(json_path) as f:
+        data = json.load(f)
+    costs = {int(t): float(v) for t, v in zip(data['test_ids'], data['test_costs'])}
+    return costs
 
 
 def save_data(df, meta):
@@ -31,29 +38,35 @@ def save_data(df, meta):
 
 if __name__ == '__main__':
 
-    data_location = r'C:\Users\crh53\OneDrive\Desktop\PHD_Experiments\E2_AMI_James\Data\Scaled_HMOF_Data.csv'
+    data_location = r'C:\Users\crh53\OneDrive\Desktop\PHD_Experiments\E2_AMI_James\Data\Scaled_HCOF_F2.csv'
     # data_location = r'../Scaled_HCOF_F2.csv'
+    cost_location = 'costs.json'
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data_file', action='store', default=data_location, help='path to data file')
-    parser.add_argument('-c', '--cost', action='store', type=float, default=1.0, help='cost of experiment')
-    parser.add_argument('-b', '--budget', action='store', type=float, default=1000.0, help='simulation budget')
+    parser.add_argument('-c', '--cost', action='store', type=str, default=cost_location, help='json file with costs')
+    parser.add_argument('-b', '--budget', action='store', type=float, default=100.0, help='simulation budget')
+    parser.add_argument('-n', '--ntests', action='store', type=int, default=2, help='# of tests to consider')
     parser.add_argument('-n', '--nthreads', action='store', type=int, default=10, help='# of parallel threads to run')
     parser.add_argument('-i', '--initial_samples', action='store', type=int, default=20, help='# of init rand samples')
     parser.add_argument('-m', '--min_samples', action='store', type=int, default=15, help='# of exp before fit')
     parser.add_argument('-a', '--acquisition', action='store', type=str, default='Thompson', help='Acquisition func')
     args = parser.parse_args()
 
-    sim_data = DataTriageCSV.load_from_path(args.data_file)
+
+    test_costs = load_costs(args.cost)
+    assert args.ntests == len(test_costs), 'Number of tests instructed and number of provided costs do not match'
+
+    sim_data = DataTriageCSV(args.data_file, n_tests=args.ntests)
 
     sim_screen = SimulatedScreenerParallel(data_params=sim_data,
-                                           test_cost=args.cost,
+                                           test_costs=test_costs,
                                            sim_budget=args.budget,
                                            nthreads=args.nthreads,
                                            num_init=args.initial_samples,
                                            min_samples=args.min_samples)
 
-    ami = BOGP.prospector(X=sim_data.X, acquisition_function=args.acquisition)
+    ami = prospector2(X=sim_data.X, costs=test_costs, acquisition_function=args.acquisition)
 
     sim_history = sim_screen.perform_screening(model=ami)
 
